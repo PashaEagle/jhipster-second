d599bdd66a2457e48c3b875eaa8b5e23492992f3
PashaEagle <kolpal17@gmail.com>
2020-03-05 21:56:00 +0200

    Initial version of jhipsterSecond generated by JHipster-6.7.1

---
+++ src/main/java/com/second/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.second.config.audit;
---
+++ src/main/java/com/second/config/dbmigrations/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * MongoDB database migrations using MongoBee.
+ */
+package com.second.config.dbmigrations;
